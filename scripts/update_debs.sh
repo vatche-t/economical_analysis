925e1ef25a466b10fc1d74e44b3874ad591269f6
vatche-t <vatche.thorossian@gmail.com>
2023-11-30 00:06:34 +0330

    updated requirements accordingly

--- scripts/update_debs.sh
+++ scripts/update_debs.sh
@@ -7,5 +7,5 @@ rm -rf requirements-*.txt
 # Install dev dependencies.
 pip install pip-tools
 # Update the deps.
-pip-compile /home/vatche/.work/sitemaps/requirements.in -o /home/vatche/.work/sitemaps/requirements.txt
+pip-compile /home/vatche/.work/economical_analysis/requirements.in -o /home/vatche/.work/economical_analysis/requirements.txt
 
